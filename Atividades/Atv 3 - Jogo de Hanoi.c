ddf2466ae37d2a3fceed6f88cdcf7263596d1a69
Matheus Endlich Silveira <126157948+MatheusIngles@users.noreply.github.com>
2024-09-11 23:41:59 -0300

    Update Atv 3 - Jogo de Hanoi.c

--- Atividades/Atv 3 - Jogo de Hanoi.c
+++ Atividades/Atv 3 - Jogo de Hanoi.c
@@ -1,3 +1,15 @@
+/* Implemente uma simulação do jogo Torre de Hanói, onde o objetivo é mover uma pilha de discos
+de um pino para outro seguindo regras específicas: apenas um disco pode ser movido por vez, e
+um disco maior não pode ser colocado sobre um disco menor. Utilize pilhas para representar
+cada um dos três pinos, onde os discos são empilhados do maior (na base) ao menor (no topo).
+Comece com todos os discos no pino inicial e mova-os para o pino de destino, utilizando o pino
+auxiliar, se necessário. O jogo deve respeitar a regra de que um disco maior não pode ser
+colocado sobre um disco menor.
+Por exemplo, se o pino inicial contém os discos [3, 2, 1], o objetivo é movê-los para o pino de
+destino, utilizando o pino auxiliar conforme as regras do jogo
+*/
+// Nome: Matheus Endlich Silveira - Matricula:202305392
+// Nome: Rafael Ferreira Bassul - Matricula: 202305395
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
